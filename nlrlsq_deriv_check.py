d6691a563ca74e97e7964a7a0ee574500da8c95a
mbsimmon <marksim747@gmail.com>
2024-02-27 15:25:20 -0600

    solved for hessian problem 2

--- nlrlsq_deriv_check.py
+++ nlrlsq_deriv_check.py
@@ -35,7 +35,7 @@ def eval_objfun( A, x, b, beta, L, flag="d2f" ):
     print(np.diag(np.sin(Ax)))
     print(b)
     # evaluate hessian
-    d2f = np.matmul(np.matmul(AT,A), np.diag(2*cosAx+  bh  )) + beta*LTL # bug!
+    d2f = np.matmul(np.matmul(AT,np.diag(np.cos(2*Ax))),A)+ np.matmul(np.matmul(AT,np.diag(np.sin(Ax))),np.matmul(np.diag(b),A)) + beta*np.identity(n)
 
     return f,df,d2f;
 
